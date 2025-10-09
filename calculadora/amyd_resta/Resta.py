56380eb390ea7dbce9f528c01e3a51469f94e5d9
Matein0411 <yungamateoand05@gmail.com>
2025-10-09 18:02:36 -0500

    Añadidos submódulos correctamente

--- calculadora/amyd_resta/Resta.py
+++
@@ -1,6 +0,0 @@
-
-def restar(* nums):
-    total = nums[0]
-    for num in nums[1:]:
-        total -= num
-    return total
--- calculadora/amyd_suma/suma.py
+++
@@ -1,6 +0,0 @@
-
-def sumar(* nums):
-    total = 0
-    for n in nums:
-        total += n
-    return total
